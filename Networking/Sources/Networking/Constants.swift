1b1de8111ac62a9b19b06f405df98c15468454e9
Manish Gupta <manishgupta.swift@gmail.com>
2024-07-04 14:48:56 -0400

    Project Commit

---
+++ Networking/Sources/Networking/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Concurrency
+//
+//  Created by Manish Gupta on 7/4/24.
+//
+
+import Foundation
+
+struct PexelsConstants {
+    static let keyID: String = "QalbuZGDWBO9jckLY6CyzgREyw3vIsNHOPWcrvpaI1apOK4O0rFRb20P"
+    static let url: String = "https://api.pexels.com/v1/search"
+    static let theURL: URL = URL(string: url)!
+    static let headerKey: String = "Authorization"
+}
